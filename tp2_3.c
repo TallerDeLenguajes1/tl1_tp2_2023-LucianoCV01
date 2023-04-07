8f47fe418b1e3989a0027e87da155876c4a6c505
LucianoCV01 <Lucianocosentino01@gmail.com>
2023-04-07 15:19:42 -0300

    Termine punto 3 y agregue su ejecutable en .gitignore

--- tp2_3.c
+++ tp2_3.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define N 5
 #define M 7
 
 
 int main(){
-int i,j;
+/*int i,j;
 int mt[N][M], *punt=mt;
 
 for(i = 0;i<N; i++)
@@ -15,6 +17,29 @@ for(i = 0;i<N; i++)
         printf("%lf", punt[i][j]);
     }
     printf("\n");
+}*/
+int i,j;
+int mt[N][M], *puntero=&mt[0][0];
+srand(time(NULL));
+/*
+while (puntero < &mt[N][M])
+{
+}*/
+
+
+for(i = 0;i<N; i++) {
+    for(j = 0;j<M; j++) {
+        //Mueve el puntero
+        /* 
+        *puntero=1+rand()%100; 
+        printf("%d ", *puntero); 
+        puntero++;
+        */ 
+        //No mueve al puntero
+        *(puntero+(i*M+j))=1+rand()%100;
+        printf("%d ", *(puntero+(i*M+j))); 
+    }
+    printf("\n"); 
 }
 
     return 0;
